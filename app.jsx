b3ad7a93b913ed008b33d0caa5fb7bfc4e8c610d
masonspacestation <71409121+masonspacestation@users.noreply.github.com>
2024-03-18 17:03:58 -0600

    nearly finished; still a little touchup needed

--- app.jsx
+++ app.jsx
@@ -1,17 +1,12 @@
 let pineapplePoints = 0
-let antiPineapplePoints = 0
+let anchoviesPoints = 0
 
 
+// pineapple stuff
 function increasePineapple() {
   pineapplePoints += 1
   console.log("Pineapple ", pineapplePoints);
   drawPineappleScore()
-}
-
-
-function increaseAntiPineapple() {
-  antiPineapplePoints += 1
-  console.log("No Thanks ", antiPineapplePoints);
 }
 
 function decreasePineapple() {
@@ -20,19 +15,61 @@ function decreasePineapple() {
   drawPineappleScore()
 }
 
-function decreaseAntiPineapple() {
-  antiPineapplePoints -= 1
-  console.log("No Thanks ", antiPineapplePoints);
+function extraPineapple() {
+  pineapplePoints += 2
+  console.log("Pineapple ", pineapplePoints);
+  drawPineappleScore()
 }
 
 function drawPineappleScore() {
   let pineappleScore = document.getElementById("pineappleScore");
-  console.log("pS ", pineappleScore)
   pineappleScore.innerText = pineapplePoints.toString();
+  checkScore()
 }
 
-function drawAntiPineappleScore() {
-  let antiPineappleScore = document.getElementById("antiPineappleScore");
-  console.log("pS ", antiPineappleScore)
-  antiPineappleScore.innerText = antiPineapplePoints.toString();
+
+// anchovies stuff
+function increaseAnchovies() {
+  anchoviesPoints += 1
+  console.log("Anchovies ", anchoviesPoints);
+  drawAnchovieScore()
+}
+
+function decreaseAnchovies() {
+  anchoviesPoints -= 1
+  console.log("Anchovies ", anchoviesPoints);
+  drawAnchovieScore()
+}
+
+function extraAnchovies() {
+  anchoviesPoints += 2
+  console.log("Anchovies ", anchoviesPoints);
+  drawAnchovieScore()
+}
+
+function drawAnchovieScore() {
+  let anchoviesScore = document.getElementById("anchoviesScore");
+  anchoviesScore.innerText = anchoviesPoints.toString();
+  checkScore()
+}
+
+// game end
+function checkScore() {
+  if (pineapplePoints >= 8) {
+    console.log("Pineapple it is")
+    window.alert("Pineapple it is")
+    resetScore()
+  } else if
+    (anchoviesPoints >= 8) {
+    console.log("Anchovies. . . Really?")
+    window.alert("Anchovies. . . Really?")
+    resetScore()
+  }
+}
+
+function resetScore() {
+  pineapplePoints = 0
+  anchoviesPoints = 0
+  drawPineappleScore()
+  drawAnchovieScore()
 }
